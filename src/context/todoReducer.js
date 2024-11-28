f21257d8baf87b0e873f23b9b650f2261cb89f2d
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-28 18:19:38 +0800

    feat:achieve delete todoItem

--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -7,6 +7,8 @@ export const todoReducer = (state, action) => {
   if(action.type==="ADD"){
     return [...state, {id: Date.now(), text: action.payload, done: false}]
   }
-
+  if(action.type==="DELETE"){
+    return [...state].filter(e=>e.id!==action.payload.id)
+  }
   return state;
 };
